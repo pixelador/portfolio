77aa4302dc04a486d6f687e1f529c8109089e094
Erik Lopez <erik@Eriks-MacBook-Pro.local>
2017-01-16 16:09:02 -0600

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,36 @@
+(function (window) {
+    'use strict';
+    var $ = window.jQuery;
+    var $nav = $("nav");
+    var $toggle = $(".toggle-menu");
+    var offset = $nav.offset().top;
+    var y_height = $nav.outerHeight(true);
+    var resizeTimer;
+
+    $(document).scroll( function() {
+        if ($(this).scrollTop() > offset) {
+            $nav.addClass("fixed_nav");
+            $("#about").css('margin-top', y_height);
+        } else {
+            $nav.removeClass("fixed_nav");
+            $("#about").removeAttr('style');
+        }
+
+    });
+
+    $toggle.click(function() {
+        $nav.toggle();
+    });
+
+    $(document).resize( function() {
+        clearTimeout(resizeTimer);
+        resizeTimer = setTimeout(function(e) {
+            if($(window).width() > 720) {
+                $nav.show();
+            } else {
+                $nav.hide();
+            }
+        }, 250);
+    });
+
+})(window);
