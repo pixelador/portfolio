88f3e43121747a22bc0a1ee6754285a25d7c6123
Erik Lopez <erik@Eriks-MacBook-Pro.local>
2017-02-08 23:25:05 -0600

    Updates layout, nav menu

--- js/main.js
+++ js/main.js
@@ -4,11 +4,12 @@
     var $nav = $("nav");
     var $toggle = $(".toggle-menu");
     var $navLink = $("nav .nav li a");
-    var offset = $nav.offset().top;
-    var y_height = $nav.outerHeight(true);
+    // var offset = $nav.offset().top;
+    // var y_height = $nav.outerHeight(true);
     var resizeTimer;
 
     $(document).scroll( function() {
+        $nav.slideUp(200);
         // if ($(this).scrollTop() > offset) {
         //     $nav.addClass("fixed_nav");
         //     $("#about").css('margin-top', y_height);
@@ -33,6 +34,19 @@
                 $nav.hide();
             }
         }, 250);
+    });
+
+    $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+        $('html, body').animate({
+            scrollTop: target.offset().top
+        }, 1000);
+        return false;
+        }
+    }
     });
 
 })(window);
